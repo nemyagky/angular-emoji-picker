af2ebbd307e999a28e667d917af0013ba391ea9c
magmiv <ivan.mag.web@gmail.com>
2021-06-23 23:41:40 +0300

    Emoji Lazy Load

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {AfterViewInit, ChangeDetectionStrategy, Component, Inject} from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
@@ -10,6 +12,32 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
 })
 
 
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+
+  constructor(@Inject(DOCUMENT) private document: Document) {
+  }
+
+  ngAfterViewInit() {
+    this.loadCss('emoji-font.css');
+  }
+
+
+  loadCss(styleName: string) {
+    const head = this.document.getElementsByTagName('head')[0];
+
+    let themeLink = this.document.getElementById(
+        'client-theme'
+    ) as HTMLLinkElement;
+    if (themeLink) {
+      themeLink.href = styleName;
+    } else {
+      const style = this.document.createElement('link');
+      style.id = 'client-theme';
+      style.rel = 'stylesheet';
+      style.href = `${styleName}`;
+
+      head.appendChild(style);
+    }
+  }
 
 }
